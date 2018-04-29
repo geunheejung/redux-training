3dbca36504bf4981f1e0c9482eeded74b61baade
junggeunhee <gh.jung1@albamapp.com>
2018-04-29 14:41:37 +0900

    Geuni:: counter reducer test 코드 추가 - egghead 4강, 5강 클리어

--- src/index.js
+++ src/index.js
@@ -1,5 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import expect from 'expect';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state;
+  }
+}
+
+// const render = () => {
+//   document.body.innerHTML = "<div>" + store.getState() +"</div>";
+// }
+
+// store.subscribe(render);
+//
+// document.addEventListener('click', () => {
+//   store.dispatch({ type: 'INCREMENT' });
+// })
+
+ReactDOM.render(<App/>, document.getElementById('root'));
+// render();
+
+// TODO counter Reducer Test Code
+/*
+expect(
+  counter(0, { type: 'INCREMENT' })
+).toEqual(1);
+
+expect(
+  counter(1, { type: 'INCREMENT' })
+).toEqual(2);
+
+expect(
+  counter(2, { type: 'DECREMENT' })
+).toEqual(1);
+
+expect(
+  counter(1, { type: 'DECREMENT' })
+).toEqual(0);
+
+expect(
+  counter(1, { type: 'SOMETHING_ELSE' })
+).toEqual(1);
+
+expect(
+  counter(undefined, {})
+).toEqual(0);
+console.log('Tests passed!');
+*/
