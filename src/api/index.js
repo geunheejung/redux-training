213c6cd128f494df4208f5db03622a2a8603c490
정근희 <>
2018-05-07 14:02:56 +0900

    Geuni:: 비동기 테스트를 위해 localStorage 관련 파일 제거 및 mookData 생성

---
+++ src/api/index.js
@@ -0,0 +1,38 @@
+import uuidV4 from 'uuid-v4';
+
+const fakeDatabase = {
+  todos: [{
+    id: uuidV4(),
+    text: 'geuni',
+    completed: true,
+  }, {
+    id: uuidV4(),
+    text: 'sky',
+    completed: true,
+  }, {
+    id: uuidV4(),
+    text: 'yangme',
+    completed: false,
+  }]
+};
+
+const delay = (ms) =>
+  new Promise(resolve => setTimeout(resolve, ms));
+
+export const fetchTodos = (filter) =>
+  delay(500).then(() => {
+    switch (filter) {
+      case 'all':
+        return fakeDatabase.todos;
+      case 'active':
+        return fakeDatabase.todos.filter(
+          t => !t.completed
+        );
+      case 'completed':
+        return fakeDatabase.todos.filter(
+          t => t.completed
+        );
+      default:
+        throw new Error(`UnKnown filter: ${filter}`);
+    }
+  });
--- src/configureStore.js
+++ src/configureStore.js
@@ -1,24 +1,34 @@
 import { createStore } from 'redux';
-import { loadState, saveState } from './localStorage';
-import _throttle from 'lodash/throttle';
 import rootReducer from './reducers/rootReducer';
 
 // Redux api인 createStore의 2번째 param 자리는 redux 상태의 초기값을 정할 수 있다.
 // store의 2번째 인자는 reducer의 초기 상태를 정할 수 있다. 이후 각 reducer마다의 상태는
 // 초기상태로 정해진 persistedState에 override된다.
 
+const addLoggingTodoDispatch = (store) => {
+  const rawDispatch = store.dispatch;
+  if (!console.group) {
+    return rawDispatch;
+  }
+
+  return (action) => {
+    console.group(`%c ---------- ${action.type} ----------`, 'color: lightblue;');
+    console.log('%c 이전 상태', 'color: gray', store.getState());
+    console.log('%c 액션', 'color: blue', action);
+    console.log('%c 다음 상태', 'color: green', store.getState());
+    console.groupEnd();
+    console.log(`--------------------`);
+    const returnValue = rawDispatch(action);
+    return returnValue;
+  };
+}
+
 const configureStore = () => {
-  const persistedState = loadState();
   const store = createStore(
     rootReducer,
-    persistedState
   );
 
-  store.subscribe(_throttle(() => {
-    saveState({
-      todos: store.getState().todos
-    });
-  }, 1000));
+  store.dispatch = addLoggingTodoDispatch(store);
 
   return store;
 }
